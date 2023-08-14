0358109c5e399a085d474920db8fb285448c0233
Juan José Briasco <jj.briasco@gmail.com>
2023-08-14 17:14:26 +0200

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,168 @@
+import Layout from "../components/Layout";
+import { skills, experiences, projects } from "../profile";
+import { ShowIcon, GithubIcon } from "../components/Icons";
+import Head from "next/head";
+
+const Index = () => (
+  <Layout>
+    <Head>
+      <title>JJ's Portfolio</title>
+    </Head>
+    {/** Header Card */}
+    <header className="row">
+      <div className="col-md-12">
+        <div className="card card-body bg-dark text-light">
+          <div className="row">
+            <div className="col-md-4">
+              <img
+                src="/profile-picture.png"
+                alt="Profile Picture"
+                className="img-fluid"
+              />
+            </div>
+            <div className="col-md-8">
+              <h1>Juan José Briasco</h1>
+              <h3>Full Stack Developer</h3>
+              <p>
+              Hello! I'm an aspiring Software Engineer and programming enthusiast. In this portfolio, I showcase my top projects that have led me to where I stand today. I hope you enjoy them!
+              </p>
+              <a href="mailto:jj.briasco@gmail.com">Contact Me!</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    {/** Second Section */}
+    <div className="row py-2">
+      <div className="col-md-4">
+        <div className="card bg-light">
+          <div className="card-body">
+            <h1>Skills</h1>
+
+            {skills.map(({ skill, percentage }, i) => (
+              <div className="py-3" key={i}>
+                <h5>{skill}</h5>
+                <div className="progress">
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: `${percentage}%` }}
+                  ></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="col-md-8">
+        <div className="card bg-light">
+          <div className="card-body">
+            <h1>Experience</h1>
+              <ul>
+            {experiences.map(({ title, description, date }, i) => (
+              <li key={i}>
+                <h3>{title}</h3>
+                <h5>{date}</h5>
+                <p>{description}</p>
+              </li>
+            ))}
+              </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    {/** Infinite Logo Slider */}
+    <div className="logos">
+      <div className="logo-slider">
+        <img src="/boxicons/bootstrap.svg" />
+        <img src="/boxicons/c-plus-plus.svg" />
+        <img src="/boxicons/css3.svg" />
+        <img src="/boxicons/figma.svg" />
+        <img src="/boxicons/git.svg" />
+        <img src="/boxicons/godot.svg" />
+        <img src="/boxicons/graphql.svg" />
+        <img src="/boxicons/heroku.svg" />
+        <img src="/boxicons/html5.svg" />
+        <img src="/boxicons/java.svg" />
+        <img src="/boxicons/javascript.svg" />
+        <img src="/boxicons/mongodb.svg" />
+        <img src="/boxicons/nodejs.svg" />
+        <img src="/boxicons/postgresql.svg" />
+        <img src="/boxicons/python.svg" />
+        <img src="/boxicons/react.svg" />
+        <img src="/boxicons/redux.svg" />
+        <img src="/boxicons/sass.svg" />
+        <img src="/boxicons/spring-boot.svg" />
+        <img src="/boxicons/tailwind-css.svg" />
+        <img src="/boxicons/tux.svg" />
+        <img src="/boxicons/typescript.svg" />
+        <img src="/boxicons/unity.svg" />
+        <img src="/boxicons/android.svg" />
+      </div>
+      <div className="logo-slider">
+        <img src="/boxicons/bootstrap.svg" />
+        <img src="/boxicons/c-plus-plus.svg" />
+        <img src="/boxicons/css3.svg" />
+        <img src="/boxicons/figma.svg" />
+        <img src="/boxicons/git.svg" />
+        <img src="/boxicons/godot.svg" />
+        <img src="/boxicons/graphql.svg" />
+        <img src="/boxicons/heroku.svg" />
+        <img src="/boxicons/html5.svg" />
+        <img src="/boxicons/java.svg" />
+        <img src="/boxicons/javascript.svg" />
+        <img src="/boxicons/mongodb.svg" />
+        <img src="/boxicons/nodejs.svg" />
+        <img src="/boxicons/postgresql.svg" />
+        <img src="/boxicons/python.svg" />
+        <img src="/boxicons/react.svg" />
+        <img src="/boxicons/redux.svg" />
+        <img src="/boxicons/sass.svg" />
+        <img src="/boxicons/spring-boot.svg" />
+        <img src="/boxicons/tailwind-css.svg" />
+        <img src="/boxicons/tux.svg" />
+        <img src="/boxicons/typescript.svg" />
+        <img src="/boxicons/unity.svg" />
+        <img src="/boxicons/android.svg" />
+      </div>
+    </div>
+
+    { /** Portfolio */}
+    <div className="row">
+      <div className="col-md-12">
+        <div className="card card-body bg-dark">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="text-center text-light"><h1>Portfolio</h1></div>
+            </div>
+
+            {
+              projects.map(({title, description, image, github, preview}, i) => (
+                <div className="col-6 col-md-4 p-2 text-center" key={i}>
+                  <div className="card h-100">
+                    <div className="overflow">
+                      <img className="card-img-top" src={`/${image}`}/>
+                    </div>
+                    <div className="card-body">
+                      <h3>{title}</h3>
+                      <p>{description}</p>
+                      <a href={github} target="_blank" className="btn btn-secondary mx-2"><GithubIcon />Github</a>
+                      <a href={preview} target="_blank" className="btn btn-secondary mx-2"><ShowIcon />Preview</a>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </Layout>
+);
+
+export default Index;
---
+++ profile.js
@@ -0,0 +1,93 @@
+export const skills = [
+  {
+    skill: "TypeScript",
+    percentage: 80,
+  },
+  {
+    skill: "Java",
+    percentage: 90,
+  },
+  {
+    skill: "SQL",
+    percentage: 70,
+  },
+  {
+    skill: "MongoDB",
+    percentage: 50,
+  },
+  {
+    skill: "React.js",
+    percentage: 85,
+  },
+  {
+    skill: "Node.js",
+    percentage: 75,
+  },
+  {
+    skill: "Next.js",
+    percentage: 80,
+  },
+  {
+    skill: "VBA",
+    percentage: 70,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Oracle Next Education",
+    description:
+      "Through the Oracle Next Education Backend Especialization, I gained a deep understanding of backend development and essential skills in building robust systems. I learned to design scalable architectures, proficiently manage databases, and apply consistent business logic. Working with key technologies like Java and Oracle databases, I've honed my ability to create effective backend solutions, preparing me for a successful career in software development.",
+    date: "Currently attending",
+  },
+  {
+    title: "Full Stack Open",
+    description:
+      "In Full Stack Open, I honed full-stack development from interactive React frontends to robust Node.js backends. Hands-on work with databases and RESTful APIs prepared me for impactful contributions in the dynamic realm of web development.",
+    date: "2022-2023",
+  },
+  {
+    title: "FreeCodeCamp.org",
+    description:
+      "Throughout my journey on FreeCodeCamp.org, I've honed my skills, progressing from the foundational aspects of web development to a proficient grasp of advanced technologies like React. From crafting basic projects to constructing interactive web applications, I've solidified my capability to turn concepts into functional realities. Each tackled challenge and every line of code written has fueled my passion for development and propelled my pursuit of programming excellence.",
+    date: "2021",
+  },
+  {
+    title: "Argentina Sé Programar",
+    description:
+      "During this course, I acquired foundational skills in web development, including HTML, CSS, and JavaScript. I applied these skills to create engaging projects like the wolf puzzle, which sparked a realization of my true passion and potential.",
+    date: "2016",
+  },
+];
+
+export const projects = [
+  {
+    title: "React calculator",
+    description: "A simple calculator using useContext to improve my React skills.",
+    image: "calculator-project.png",
+    github: "https://github.com/JJBandana/react-calculator",
+    preview: "https://jjbandana.github.io/react-calculator/"
+  },
+  {
+    title: "NFT Preview Card",
+    description: "A Frontend Mentor Challenge I did to put my HTML and CSS skills on test.",
+    image: "nft-project.jpg",
+    github: "https://github.com/JJBandana/nft-preview-card",
+    preview: "https://jjbandana.github.io/nft-preview-card/"
+  },
+  {
+    title: "Survey Project",
+    description: "Survey Page made for FreeCodeCamp with only HTML and CSS.",
+    image: "survey-project.jpg",
+    github: "https://codepen.io/jjbandana/pen/LYdgJxm",
+    preview: "https://codepen.io/jjbandana/full/LYdgJxm"
+  },
+  {
+    title: "Wolf Puzzle",
+    description:
+      "A puzzle I made with HTML, CSS and JavaScript without libraries when I was 17 years old.",
+    image: "puzzle-project.png",
+    github: "https://github.com/JJBandana/rompecabezas",
+    preview: "https://jjbandana.github.io/rompecabezas/"
+  },
+];
